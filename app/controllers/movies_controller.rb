79cc67bee45fbc46257e7e8f7c650fd260c1a25a
RobynSpaulding <robynm27@gmail.com>
2022-09-06 20:17:13 -0700

    adds new movie model instance, updates existing movie model instance, adds director and english to create method and update method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,6 +27,8 @@ class MoviesController < ApplicationController
     movie.title = params["title"] || movie.title
     movie.year = params["year"] || movie.year
     movie.plot = params["plot"] || movie.plot
+    movie.director = params["director"] || movie.director
+    movie.english = params["english"] || movie.english
      
     movie.save
     render json: movie.as_json
