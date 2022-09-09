1584b1bee0639f789e071858a377d5f8e524f206
RobynSpaulding <robynm27@gmail.com>
2022-09-08 19:22:22 -0700

    adds database (had to delete to reinstall terminal stuff) migrates and seeds database, adds validations to actor model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock", age: 47, gender: "M"})
 actor.save
 
 Movie.create(title: "Monty Python and the Holy Grail", year: 1975, plot: "A comedic send-up of the grim circumstances of the Middle Ages as told through the story of King Arthur and framed by a modern-day murder investigation. When the mythical king of the Britons leads his knights on a quest for the Holy Grail, they face a wide array of horrors, including a persistent Black Knight, a three-headed giant, a cadre of shrubbery-challenged knights, the perilous Castle Anthrax, a killer rabbit, a house of virgins, and a handful of rude Frenchmen.
