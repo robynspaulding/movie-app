f39f7929b1812a7617637cde9c9f202fc5f8e38d
RobynSpaulding <robynm27@gmail.com>
2022-09-11 13:21:40 -0700

    In the movie-app, follow the authentication guide to implement users and authentication! Create a few users by sending a POST request in your .http file. Add a migration to create an admin attribute on a User (it can default to false). An admin user will be allowed to create/update/destroy Actor and Movie information, and a non-admin user will not. Make an authenticate_admin method in the application controller similar to the authenticate_user method we've used before. Create a before_action at the top of the actors_controller and the movies_controller that will allow users to see actor/movie data, but not modify or create it. However I was unable to create an admin user, all the tests i did remained admin false(I think becuase of the default setting).

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  before_action :authenticate_admin, except: [:index, :show]
 
   def index 
     movies = Movie.all
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #Actors
   get "actors"=>"actors#index"
   get "/actors/:id" => "actors#show"
   post "actors" => "actors#create"
   patch "actors/:id" => "actors#update"
   delete "actors/:id" => "actors#destroy"
 
-
+  #Movies
   get "movies"=>"movies#index"
   get "/movies/:id" => "movies#show"
   post "movies" => "movies#create"
   patch "movies/:id" => "movies#update"
   delete "movies/:id" => "movies#destroy"
+
+  #Users
+  post "/users" => "users#create"
+
+  #Sessions
+  post "/sessions" => "sessions#create"
 end
