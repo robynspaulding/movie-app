27f2546745d2aa983315a17e5e2a2febc5c480ca
RobynSpaulding <robynm27@gmail.com>
2022-09-13 07:50:05 -0700

    adds belongs_to and has_many relationships to the movie_genre.rb, movie.rb, and genre.rb files

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :year, presence: true
   validates :plot, length: { minimum: 10}
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
